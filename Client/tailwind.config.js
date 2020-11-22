33827db8ee27d233b41f9b99f8ccfaabb09cb1a3
Layla Porter <layla.porter@gmail.com>
2020-11-22 13:26:53 +0000

    Content additions and layout updates

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,21 +1,21 @@
 ﻿module.exports = {
-    theme: {
-        extend: {
-            colors: {
-                laylateal: '#0c6474',
-                laylanavy: '#31374d',
-                laylagold: '#c8903e'
-            },
-            fontFamily: {
-                'deco': ['RitzyNormal', 'Georgia', 'serif'],
-                'decoserif': ['Oswald', 'roboto', 'sans']
+	theme: {
+		extend: {
+			colors: {
+				laylateal: '#0c6474',
+				laylanavy: '#31374d',
+				laylagold: '#c8903e'
+			},
+			fontFamily: {
+				'deco': ['RitzyNormal', 'Georgia', 'serif'],
+				'decoserif': ['Oswald', 'roboto', 'sans']
 
-            }
-        }
-    },
-    variants: {
-        extend: {
-            textColor: ['active'],
-        }
-    }
+			}
+		}
+	},
+	variants: {
+		extend: {
+			textColor: ['active'],
+		}
+	}
 };
